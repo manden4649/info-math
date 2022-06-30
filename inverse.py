4c69fcd08ce690c032b1920cd395a01dfafb521c
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-06-30 20:28:59 +0900

    update (笑)

--- inverse.py
+++ inverse.py
@@ -1,8 +1,9 @@
 import numpy as np
 data = [[0,0],[0,0]]
-for i in range(2):
-    for j in range(2):
-        data[i][j] = input(f"plese input {i+1}:{j+1} = ")
+
+for i in range(2) :
+    for j in range(2) :
+    data[i][j] = input(f"plese input {i+1}:{j+1} = ")
 
 
 
