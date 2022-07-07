4d79877b7dc5a49905056db10abdaaaa56a15231
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-07-07 17:00:24 +0900

    Functionalization

--- inverse.py
+++ inverse.py
@@ -1,29 +1,35 @@
 import numpy as np
 
-data = np.array([[0,0],[0,0]])
+
+def inverse(rawdata,mod):
+    data = np.array(rawdata)
+
+    #--- 行列式計算 ---#
+    det = np.linalg.det(data)
+    det_inv = int(det % mod) #modかける
+
+
+    #--- ひっくり返す(いい感じじゃない) ---#
+    tmp = data[0][0]
+    data[0][0] = data[1][1]
+    data[1][1] = tmp
+
+    tmp = data[0][1]
+    data[0][1] = data[1][0]*(-1)
+    data[1][0] = tmp*(-1)
+
+    data_inv = np.dot(data, det_inv) % mod
+
+    return data_inv
+
+data = [[0,0],[0,0]]
 for i in range(2):
     for j in range(2):
         data[i][j] = input(f"plese input {i+1}:{j+1} = ")
 
 mod = int(input("pls input mod = "))
 
-#--- 行列式計算 ---#
-det = np.linalg.det(data)
-det_inv = int(det % mod) #modかける
-
-
-#--- ひっくり返す(いい感じじゃない) ---#
-tmp = data[0][0]
-data[0][0] = data[1][1]
-data[1][1] = tmp
-
-tmp = data[0][1]
-data[0][1] = data[1][0]*(-1)
-data[1][0] = tmp*(-1)
-
-data_inv = np.dot(data, det_inv) % mod
-
 print(data)
-print(det_inv)
 
-print(data_inv)
+
+print(inverse(data,mod))
